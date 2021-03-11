3201d33c382da31f211219e6015b30c215ab129f
fabioemoutinho <fabioemoutinho@gmail.com>
2021-03-11 00:09:45 -0300

    basic map and character movement feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectPokemon } from './pokemon/pokemon.actions';
@@ -11,11 +15,70 @@ import { PokemonService } from './pokemon/pokemon.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  currentPositionX = 16;
+  currentPositionY = 33;
+
   constructor(
     private router: Router,
     private store: Store,
     private pokemonService: PokemonService
   ) {}
+
+  @HostListener('document:keydown', ['$event'])
+  move(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'Down': // IE/Edge specific value
+      case 'ArrowDown':
+      case 's':
+        this.moveDown();
+        break;
+      case 'Up': // IE/Edge specific value
+      case 'ArrowUp':
+      case 'w':
+        this.moveUp();
+        break;
+      case 'Left': // IE/Edge specific value
+      case 'ArrowLeft':
+      case 'a':
+        this.moveLeft();
+        break;
+      case 'Right': // IE/Edge specific value
+      case 'ArrowRight':
+      case 'd':
+        this.moveRight();
+        break;
+    }
+  }
+
+  getBackgroundPosition(): string {
+    return `${-this.currentPositionX * 16 + 64}px ${
+      -this.currentPositionY * 16 + 64
+    }px`;
+  }
+
+  moveLeft(): void {
+    if (this.currentPositionX > 0) {
+      this.currentPositionX -= 1;
+    }
+  }
+
+  moveRight(): void {
+    if (this.currentPositionX < 33) {
+      this.currentPositionX += 1;
+    }
+  }
+
+  moveUp(): void {
+    if (this.currentPositionY > 0) {
+      this.currentPositionY -= 1;
+    }
+  }
+
+  moveDown(): void {
+    if (this.currentPositionY < 47) {
+      this.currentPositionY += 1;
+    }
+  }
 
   battle(): void {
     this.store.dispatch(
