c1002ce4e05b7b3c0c91e5f4c9e7cb57b8fa7052
fabioemoutinho <fabioemoutinho@gmail.com>
2021-05-23 23:57:14 -0300

    styling ab, start select buttons

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { selectAllPokemon } from '../pokemon/pokemon.selectors';
 
@@ -13,13 +12,9 @@ export class PokedexComponent {
   readonly pokemon$ = this.store.select(selectAllPokemon);
   selected?: number = undefined;
 
-  constructor(private router: Router, private store: Store) {}
+  constructor(private store: Store) {}
 
   select(i: number): void {
     this.selected = i;
-  }
-
-  map(): void {
-    this.router.navigate(['map']);
   }
 }
